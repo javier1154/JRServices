11132e77e09c3f467a688899b7ccc66f6b308d21
Javier Velasquez <Javiervelasquez1992@gmail.com>
2020-01-03 10:05:04 -0400

    subiendo pruebas

---
+++ wp-content/themes/flatone/resources/js/parallax.js
@@ -0,0 +1,9 @@
+
+jQuery(document).ready(function(){
+		jQuery('.top_wrapper').parallax("50%", 0.4);
+		jQuery('.features_wrapper').parallax("50%", 0.2);
+		jQuery('.subscribe_wrapper').parallax("50%", 0.2);
+		jQuery('.clients_wrapper').parallax("50%", 0.2);
+		jQuery('.team_wrapper').parallax("50%", 0.2);			
+		jQuery('.faq_wrapper').parallax("50%", 0.2);
+})
